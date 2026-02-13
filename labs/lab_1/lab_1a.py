8b2afc083c11c39413920220df41cead14209b6d
Luis Padilla <luispadilla@icloud.com>
2026-02-13 13:03:47 -0600

    Step 2 for lab1a
    added self discription

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Luis" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print(f"{name},Hello everyone, I am junior from Indiana. I enjoy building arduino projects. My friends and I are currently working on building a go kart. ")
 if __name__ == "__main__":
     main()
